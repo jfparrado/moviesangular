acb6ff32ee1350ddb790c8f4fa79e1aafb1647d5
jfparrado <jnsparrado@hotmail.com>
2023-04-19 16:33:16 -0500

    notfound added

--- src/app/components/template-many-movies/template-many-movies.component.ts
+++ src/app/components/template-many-movies/template-many-movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GeneralMovie } from '../../interfaces/interfaces';
 
 @Component({
@@ -6,11 +6,25 @@ import { GeneralMovie } from '../../interfaces/interfaces';
   templateUrl: './template-many-movies.component.html',
   styleUrls: ['./template-many-movies.component.css']
 })
-export class TemplateManyMoviesComponent {
-  @Input() titulo: string ="";
-  @Input() movies: GeneralMovie[] =[]; 
+export class TemplateManyMoviesComponent implements OnInit {
+  @Input() titulo: string = "";
+  @Input() movies: GeneralMovie[] = [];
+
+  showNoResults: boolean = false;
+  showLoading: boolean = true;
 
   constructor() { }
+
+  ngOnInit() {
+    if (this.movies.length === 0) {
+      setTimeout(() => {
+        this.showNoResults = true;
+        this.showLoading = false;
+      }, 3000);
+    } else {
+      this.showLoading = false;
+    }
+  }
 
   getMoviesWithPosters(): GeneralMovie[] {
     return this.movies.filter(movie => movie.poster_path);
